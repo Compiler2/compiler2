8c733a164dcb2aa3258e3bc1f09a75104da19e44
dejangrubisic <grubisic.dejan@yahoo.com>
2023-02-17 18:20:45 -0600

    Adding transformer to demo_hpctoolkit

--- compiler2_service/demo_hpctoolkit.py
+++ compiler2_service/demo_hpctoolkit.py
@@ -9,39 +9,64 @@ to use the bazel build system. Usage:
 
 It is equivalent in behavior to the demo.py script in this directory.
 """
+import argparse
 import logging
-import os
-import pdb
 import pickle
-import subprocess
 from pathlib import Path
-from typing import Iterable
-
-import gym
-import hatchet as ht
-
-from compiler_gym.datasets import Benchmark, BenchmarkUri, Dataset
-from compiler_gym.spaces import Reward
-from compiler_gym.third_party import llvm
-from compiler_gym.util.logging import init_logging
-from compiler_gym.util.registration import register
-from compiler_gym.util.runfiles_path import runfiles_path, site_data_path
-from compiler_gym.service.connection import ServiceError
-import compiler2_service
-
-from agent_py.rewards import hpctoolkit_reward
-
-
-import dgl
 import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+
+from compiler_gym.util.logging import init_logging
+from compiler_gym.service.connection import ServiceError
+
+import compiler2_service
+from compiler2_service.model.transformer.graph_encoder.dgl_dataset import GraphormerDGLDataset
+from compiler2_service.model.transformer.graphormer_encoder import GraphormerEncoder
+from compiler2_service.model.transformer.graphormer_transformer import GraphormerTransformer
+from compiler2_service.agent_py.rewards import hpctoolkit_reward
+
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--arch', type=str, default='transformer', help='Architecture transformer or encoder.'
+)
+parser.add_argument(
+    "--dim_emb", type=int, default=64, help="Embedding dimension."
+)
+parser.add_argument(
+    "--heads", type=int, default=2, help="Number of heads."
+)
+parser.add_argument(
+    "--encoder_layers", type=int, default=3, help="Number of encoder layers."
+)
+parser.add_argument(
+    "--decoder_layers", type=int, default=3, help="Number of decoder layers."
+)
+parser.add_argument(
+    "--dropout", type=float, default=0.1, help="Dropout."
+)
+parser.add_argument(
+    "--epochs", type=int, default=500, help="Number of decoder layers."
+)
+
 
 
 
 def main():
+    args = parser.parse_args()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
     # Use debug verbosity to print out extra logging information.
-    init_logging(level=logging.DEBUG)
+    init_logging(level=logging.CRITICAL)
 
     reward_metric = hpctoolkit_reward.RewardPickle.reward_metric
+
+    dataset = {'graphs':[], 'labels':[]}
+
     observation_spaces = 'hpctoolkit_dgl_pickle' # 'hpctoolkit_pickle'
 
     # Create the environment using the regular gym.make(...) interface.
@@ -56,19 +81,23 @@ def main():
                 print("AGENT: Timeout Error Reset")
                 continue
                 
-        
+            actions = [0]
             for i in range(2):
                 print("Main: step = ", i)
                 try:
+                    action = env.action_space.sample() 
+                    print(f'{action}-----------------------------------------------------')
+
                     observation, reward, done, info = env.step(
-                        action=env.action_space.sample(),
+                        action=action,
                         observation_spaces=[observation_spaces],
                         # reward_spaces=["perf_reward"],
                     )
                 except ServiceError:
                     print("AGENT: Timeout Error Step")
                     continue
-                
+                actions.append(action + 2) # + 2 for start/end token
+
                 print(reward)
                 print(info)
 
@@ -80,12 +109,75 @@ def main():
                     dgl = pickle.loads(observation[0])
                     print(dgl)
 
+
+                dataset["graphs"].append(dgl)
+                dataset["labels"].append(actions)
                 breakpoint()
 
+            actions.append(1)
 
-                if done:
-                    env.reset()
 
+    dgl_dataset = GraphormerDGLDataset(graphs=dataset["graphs"], labels=dataset["labels"], device=device)
+    # with open('dgl.pkl', 'rb') as handle: dgl_dataset = pickle.load(handle)    
+    
+    train_data = dgl_dataset.get_train()
+    valid_data = dgl_dataset.get_valid()
+    
+    # with open('dgl.pkl', 'wb') as handle: pickle.dump(dgl_dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+    if args.arch == 'encoder':
+        model = GraphormerEncoder(
+            num_classes=dgl_dataset.num_classes,
+        ).to(device=device)
+    elif args.arch == 'transformer':
+        model = GraphormerTransformer(
+            num_nodes=dgl_dataset.max_nodes,
+            num_classes=dgl_dataset.num_classes,
+            dim_model=args.dim_emb, 
+            num_heads=args.heads, 
+            num_encoder_layers=args.encoder_layers, 
+            num_decoder_layers=args.decoder_layers, 
+            dropout_p=args.dropout
+        ).to(device)
+    else:
+        print('Argument arch must be transformer or encoder')
+        exit(1)
+
+    opt = torch.optim.SGD(model.parameters(), lr=0.01)
+    loss_fn = nn.CrossEntropyLoss()
+
+    train_loss_list, validation_loss_list = model.fit(
+        opt=opt, 
+        loss_fn=loss_fn, 
+        epochs=args.epochs, 
+        train_dataloader=train_data,
+        valid_dataloader=valid_data,
+    )
+
+    plt.plot(train_loss_list, label = "Train loss")
+    plt.plot(validation_loss_list, label = "Validation loss")
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Loss vs Epoch')
+    plt.legend()
+    plt.savefig(Path(__file__).parent/'result.png')
+
+
+    def predict(dataset):
+        y_predicted = model.predict(dataset['x'])
+
+        for y_pred, y in zip(y_predicted, dataset['y']):
+            print(f'y_pred: {y_pred} -> y: {y.tolist()}')
+
+    print('Train data-----------------------------------------------------')
+    predict(train_data)
+
+    print('Validation data------------------------------------------------')
+    predict(valid_data)
+    
+    print('Test data------------------------------------------------------')
+    predict(dgl_dataset.get_test())            
 
 if __name__ == "__main__":
     main()
