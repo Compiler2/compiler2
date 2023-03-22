f903db2454f7ab1316a234e1f02c8498317591f4
dejangrubisic <grubisic.dejan@yahoo.com>
2023-03-22 14:46:54 -0500

    Deleting poj104_test/train_old

--- compiler2_service/benchmarks/poj104_train_old/code/78_46.c
+++
@@ -1,7 +0,0 @@
-#include <header.h>
-
-int main()
-{
-    int i,j,z,q,s,l;
-    printf("l 50\nq 40\nz 20\ns 10\n");
-}
--- compiler2_service/benchmarks/poj104_train_old/code/78_48.c
+++
@@ -1,9 +0,0 @@
-#include <header.h>
-
-main()
-{
-  char z,q,s,l;
-   printf("%c %d\n%c %d\n%c %d\n%c %d\n",'l',50,'q',40,'z',20,'s',10);
-   getchar();
-   getchar();  
-}
